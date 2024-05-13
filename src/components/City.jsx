91391091c984d2ca5710cb97a306dd8691899a39
Your name <Your email>
2024-05-13 16:16:13 +0200

    icons and more

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -21,6 +21,11 @@ function City() {
       const data = await response.json();
       if (data.weather) {
         setCityNames(data.weather);
+
+        // const sunriseDate = new Date(data.sunrise * 1000);
+        // const sunrise = sunriseDate.toLocaleTimeString("en-US");
+        // const sunsetDate = new Date(data.sunset * 1000);
+        // const sunset = sunsetDate.toLocaleTimeString("en-US");
       }
     } catch (error) {
       console.error("Error fetching cities:", error);
@@ -52,23 +57,36 @@ function City() {
         {cityNames.map((city) => (
           <div
             key={city.cityName}
-            className="bg-gray-100 rounded-md shadow-md m-2 p-6 w-auto flex flex-col justify-around items-center"
+            className="bg-blue-200 rounded-md shadow-md m-2 p-6 w-auto flex flex-col justify-around items-center"
           >
             <div className="flex flex-col items-center justify-center">
-              <p className="font-semibold">{city.cityName}</p>
-              <p>{city.description}</p>
-              <div className="flex items-center justify-center">
-                <img
-                  className="w-8 h-8 mr-2"
-                  src={`images/${city.main}.png`}
-                  alt="Weather Icon"
-                />
-                <div className="flex">
-                  <p className="mr-2">{city.tempMin}°C</p>
-                  <span>-</span>
-                  <p className="ml-2">{city.tempMax}°C</p>
-                </div>
-              </div>
+              <p className="font-semibold">
+                {city.cityName
+                  .split(" ")
+                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(" ")}
+              </p>
+              <img
+                className="w-16 h-16"
+                src={`images/${city.icon}.png`}
+                alt="Weather Icon"
+              />
+
+              <p>{city.sunrise}</p>
+              <p>{city.sunset}</p>
+              <p>
+                {city.description.charAt(0).toUpperCase() +
+                  city.description.slice(1)}{" "}
+              </p>
+              <p>Current temp: {city.temp}°C</p>
+              <p>Feels like: {city.feels_like}°C</p>
+              <p className="mr-2">Min temp: {city.tempMin}°C</p>
+              <p className="ml-2">Max temp: {city.tempMax}°C</p>
+              <p>Humidity: {city.humidity}%</p>
+              <p>Wind: {city.wind} m/s</p>
+              <p>Clouds: {city.clouds}%</p>
+              <p>Rain: {city.rain} mm</p>
+              <p>Snow: {city.snow} mm</p>
             </div>
             <button
               className="mt-8 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:bg-red-600"
