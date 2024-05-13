728f78cfa92b28155bd9112fc05788f2c94a693e
Your name <Your email>
2024-05-13 10:51:18 +0200

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}",],
+  content: ['./pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
 
